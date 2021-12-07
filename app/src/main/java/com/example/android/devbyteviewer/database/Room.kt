b421849ccb9eb9f0bb7d365e1ba8d88e05e8313d
Smrza <j_smrza@utb.cz>
2021-12-07 19:56:51 +0100

    update room

--- app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -15,3 +15,18 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface VideoDao {
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll( videos: List<DatabaseVideo>)
+}
